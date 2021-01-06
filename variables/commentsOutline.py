e18f68ed40804c1ef5c17ac800991e9068ffbedb
Kevialito <Kevialito@users.noreply.github.com>
2021-01-06 12:37:46 -0600

    ITA
    ITA

---
+++ variables/commentsOutline.py
@@ -0,0 +1,19 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#fdfdsfdsfdsgsfds
+
+
+
+'''ssssss
+fdsfsdfsdfdsfsdfsd
+yuhyuhyuhuyhuyhuyhuyuh
+'''
+
+
+
+var=5 #ddsfdsfsdfsdfsdfsdfsef
+
+
